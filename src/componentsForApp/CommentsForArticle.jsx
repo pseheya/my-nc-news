af783304116d2642eddba49ceca8d033d7baff99
Oksana Herasymenko <oksanagerasymenko@Mac.mynet>
2024-12-17 16:29:27 +0000

    task 8 completed

--- src/componentsForApp/CommentsForArticle.jsx
+++ src/componentsForApp/CommentsForArticle.jsx
@@ -3,20 +3,37 @@ import apiFunctions from "../fetchingData/fetchData";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 import { Box, Button, Text } from "@chakra-ui/react";
+import LeaveCommentForArticle from "./LeaveCommentForArticle";
 
 export default function CommentsForArticle() {
   const { article_id } = useParams();
-  const [comments, setComments] = useState(null);
+  const [comments, setComments] = useState([]);
   const [isLoading, setLoading] = useState(false);
-  const [addComment, setAddComment] = useState(false);
+  const [addCLick, setAddClick] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [commentsPerPage, setCommentsPerPage] = useState(10);
+  const [hasMoreComments, setHasMoreComments] = useState(true);
+
+  function handleClick() {
+    setAddClick(!addCLick);
+  }
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
 
   useEffect(() => {
     setLoading(true);
     apiFunctions
-      .getCommentsByArticleId(article_id)
+      .getCommentsByArticleId(article_id, currentPage, commentsPerPage)
       .then((data) => {
         if (data && data.comment) {
           setComments(data.comment);
+        }
+        if (data.comment.length < commentsPerPage) {
+          setHasMoreComments(false);
+        } else {
+          setHasMoreComments(true);
         }
       })
       .catch((err) => {
@@ -25,22 +42,40 @@ export default function CommentsForArticle() {
       .finally(() => {
         setLoading(false);
       });
-  }, [article_id]);
+  }, [article_id, currentPage]);
 
   if (isLoading) {
     return <Text>Loading, please wait...</Text>;
   }
 
-  if (comments) {
-    return (
-      <Box mt="4" p="4" borderWidth="1px" borderRadius="lg" bg="gray.50">
-        <Box p="2" borderBottom="1px solid lightgray">
-          <Button bg="grey" borderBottom="1px solid lightgray">
-            Add Comment
-          </Button>
-        </Box>
+  return (
+    <Box
+      mt="4"
+      p="4"
+      borderWidth="1px"
+      borderRadius="lg"
+      bg="gray.50"
+      minHeight="400px"
+    >
+      <Box p="2" borderBottom="1px solid lightgray">
+        <Button
+          onClick={handleClick}
+          bg="grey"
+          borderBottom="1px solid lightgray"
+        >
+          Add Comment
+        </Button>
+        {addCLick && (
+          <LeaveCommentForArticle
+            article_id={article_id}
+            setAddClick={setAddClick}
+            setCommentsUpdated={setCommentsUpdated}
+          />
+        )}
+      </Box>
 
-        {comments.map((comment) => (
+      {comments.length > 0 ? (
+        comments.map((comment) => (
           <Box
             key={comment.comment_id}
             p="2"
@@ -53,17 +88,28 @@ export default function CommentsForArticle() {
             </Text>
             <Text>Votes: {comment.votes}</Text>
           </Box>
-        ))}
+        ))
+      ) : (
+        <Text>No comments available for this article.</Text>
+      )}
+
+      <Box display="flex" justifyContent="center" mt="4">
+        <Button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+          bg="grey"
+        >
+          Previous
+        </Button>
+        <Text mx="2">Page {currentPage}</Text>
+        <Button
+          onClick={() => handlePageChange(currentPage + 1)}
+          bg="grey"
+          disabled={!hasMoreComments}
+        >
+          Next
+        </Button>
       </Box>
-    );
-  } else {
-    return (
-      <Box>
-        <Box p="2" borderBottom="1px solid lightgray">
-          <Button bg="grey">Add Comment</Button>
-        </Box>
-        <Text>No comments for this article</Text>
-      </Box>
-    );
-  }
+    </Box>
+  );
 }
---
+++ src/componentsForApp/LeaveCommentForArticle.jsx
@@ -0,0 +1,88 @@
+import { Box, Text, Input, Button, Textarea } from "@chakra-ui/react";
+import { Field } from "../components/ui/field";
+import { useEffect, useState } from "react";
+import apiFunction from "../fetchingData/fetchData";
+import postFunctions from "../fetchingData/postData";
+
+export default function LeaveCommentForArticle({
+  article_id,
+  setAddClick,
+  setComments,
+}) {
+  const [message, setMessage] = useState("");
+  const [submitted, setSubmited] = useState("");
+  const [newComment, setNewComments] = useState({
+    username: "",
+    body: "",
+  });
+
+  const handleCommentInput = (e) => {
+    const { name, value } = e.target;
+    setNewComments((prevComment) => ({
+      ...prevComment,
+      [name]: value,
+    }));
+  };
+
+  const postComment = () => {
+    const username = newComment.username.trim();
+    if (!newComment.username || !newComment.body) {
+      setMessage("You need to enter your username and comment");
+      return;
+    }
+
+    postFunctions
+      .postCommentByArticleId(username, newComment.body, article_id)
+      .then((newCommentData) => {
+        setSubmited("Your comment has been submitted!");
+        setMessage("");
+        setAddClick(false);
+        return newCommentData.comment;
+      })
+      .then((data) => {
+        setComments((prevComments) => [...prevComments, data]);
+      })
+      .catch((err) => {
+        setMessage(
+          "Your username does not exist, you need to register to leave a comment."
+        );
+      });
+  };
+
+  return (
+    <Box mt="4" p="4" borderBottom="1px solid lightgray">
+      <Field label="Username" required errorText="This field is required">
+        <Input
+          name="username"
+          value={newComment.username}
+          placeholder="Enter your username"
+          onChange={handleCommentInput}
+        />
+      </Field>
+      <Field label="Comment" required errorText="Field is required">
+        <Textarea
+          value={newComment.body}
+          name="body"
+          placeholder="Enter your comment"
+          required
+          onChange={handleCommentInput}
+        />
+      </Field>
+      <Box display="flex" gap="1" mt="2">
+        <Button bg="grey" onClick={postComment}>
+          Submit
+        </Button>
+      </Box>
+      {message && (
+        <Text mt="2" color="red.500">
+          {message}
+        </Text>
+      )}
+      {submitted && (
+        <Text mt="2" color="green.500">
+          {submitted}
+        </Text>
+      )}
+    </Box>
+  );
+}
